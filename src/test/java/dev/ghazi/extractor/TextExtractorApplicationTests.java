8ee9b01ba8063dc0056d900c6ae03addbb67cb67
Ghazi Hakim <ghazi.hakim@alenia.io>
2025-05-05 11:02:45 +0200

    first commit

---
+++ src/test/java/dev/ghazi/extractor/TextExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ghazi.extractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
